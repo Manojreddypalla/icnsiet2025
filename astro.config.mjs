6f95dad0e7b5a3017545fcd44695c7aec03760b3
Manojreddypalla <Manojreddypalla@gmail.com>
2025-03-06 14:39:38 +0530

    some importent changes for depolyment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind()]
 });
